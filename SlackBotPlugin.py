010e4c700a6a1927f184fc4e1af5b2521d886fd5
calic0cat <suzuki11235813@gmail.com>
2020-01-26 19:20:46 +0900

    開閉可能か判断する

--- SlackBotPlugin.py
+++ SlackBotPlugin.py
@@ -4,22 +4,6 @@ import re
 import RPi.GPIO as GPIO
 import time
 lock = True
-# GPIO.setmode(GPIO.BCM)
-
-# #GPIO4を制御パルスの出力に設定
-# gp_out=4
-# GPIO.setup(gp_out, GPIO.OUT)
-# servo = GPIO.PWM(gp_out, 50)
-#「GPIO4出力」でPWMインスタンスを作成する。
-#GPIO.PWM( [ピン番号] , [周波数Hz] )
-#SG92RはPWMサイクル:20ms(=50Hz), 制御パルス:0.5ms〜2.4ms, (=2.5%〜12%)。
-
-
-# ピンを出力に設定
-# pi.set_mode(servo, pigpio.OUTPUT)
-#パルス出力開始。　servo.start( [デューティサイクル 0~100%] )
-#とりあえずゼロ指定だとサイクルが生まれないので特に動かないっぽい？
-# servo.start(0)
 
 # 「カギ開けて」「解錠して」等に反応するようにします
 @listen_to(u'(鍵|カギ)+.*(開|あけ|空け)+')
@@ -47,9 +31,6 @@ def openKeyOrder(message, *something):
       GPIO.cleanup()
     else:
       message.reply(u'鍵はすでに開いています。')
-    # # 命令を出したユーザ名を取得することもできます。
-    # userID = message.channel._client.users[message.body['user']][u'name']
-    # print userID + 'さんの命令でカギを開けます'
 
 # 「鍵閉めて」「施錠」等の場合はこちら
 @listen_to(u'(鍵|カギ)+.*(閉|しめ|締め)+')
